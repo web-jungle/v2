abda3e9391bb3c676f29635ffc5c6f6c2eecdcaa
Agence Web La Capsule <webjungle66@gmail.com>
2025-05-07 00:00:46 +0200

    ok

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,16 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "../src/generated/prisma";
 
 // PrismaClient est attaché au global object en développement pour éviter
 // d'épuiser la limite de connexions à la base de données pendant le hot-reloading
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-  })
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "error", "warn"]
+        : ["error"],
+  });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
